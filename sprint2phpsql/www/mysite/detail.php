aa79e82fd5e132a8d75f65a44142d095418fb568
carlos <cpazoss22@fpcoruna.afundacion.org>
2023-10-25 19:02:02 +0200

    Ejercicio2

---
+++ sprint2phpsql/www/mysite/detail.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<?php
+  $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+?>
+</head>
+  <body>
+    <?php
+	if (!isset($_GET['id'])) {
+	 die('No se ha especificado un juego');
+	}
+	$id = $_GET['id'];
+	$query = 'SELECT * FROM tJuegos WHERE id='.$id;
+	$result = mysqli_query($db, $query) or die('Query error');
+	$only_row = mysqli_fetch_array($result);
+	echo '<img src=''' .$only_row['url_imagen'] . '"id="game-image"/>';
+	echo "<h1>" .$only_row['nombre'] ."</h1>";
+    	echo '<p>' .$only_row['genero'] ."</p>";
+	echo '<p>' .$only_row['precio'] ."</p>";
+	?>
+    <h3>Comentarios:</h3>
+    <ul>
+      <?php
+	$query2 = "SELECT * FROM tComentarios WHERE juegos_id=".$id;
+	$result2 = mysqli_query($db, $query2) or die("Query error");
+	while ($row = mysqli_fetch_array($result2)) {
+	  echo "<li>".$row["comentario"]."</li>";
+      }
+      mysqli_close($db);
+      ?>
+    </ul>
+  </body>
+</html>
--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,10 +1,23 @@
 <html>
 <head>
 <?php
-	$db = mysqli_connect('localhost', 'carlos', '5677', 'mysitedb') or die('Fail');
+	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 </head>
-<body>
-<h1>Conexión establecida</h1>
+	<body>
+	 <h1>Conexión establecida</h1>
+	 <?php
+	  // Lanzar una query
+	  $query = 'SELECT * FROM tJuegos';
+	  $result = mysqli_query($db, $query) or die('Query error');
+	  // Recorrer el resultado
+	  while ($row = mysqli_fetch_array($result)) {
+		echo $row['nombre'];
+		echo '<br>';
+		echo $row[2];
+		echo '<br>';
+	}
+mysqli_close($db);
+?>
 </body>
 </html>
